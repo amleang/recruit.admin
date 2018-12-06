38d5eddb05e8dc78adbdf6b6d5e6b89b06e9f776
少游 <shaoyou@jzez100.com>
2018-12-07 00:24:11 +0800

    update

--- src/pages/recruit/list/form.vue
+++ src/pages/recruit/list/form.vue
@@ -1,73 +1,201 @@
 <template>
-  <el-dialog title="招工表单" :visible.sync="formDialog" top="20vh" width="1000px" :modal-append-to-body="false" :fullscreen="true" :close-on-click-modal="false" :before-close="handleClose">
+  <el-dialog
+    title="招工表单"
+    :visible.sync="formDialog"
+    top="20vh"
+    width="1000px"
+    :modal-append-to-body="false"
+    :fullscreen="true"
+    :close-on-click-modal="false"
+    :before-close="handleClose"
+  >
     <el-card>
-      <el-form label-width="180px" ref="form">
+      <el-form
+        label-width="180px"
+        ref="form"
+      >
         <el-form-item label="标题">
-          <el-input v-model="form.name" size="small" maxlength="50"></el-input>
+          <el-input
+            v-model="form.name"
+            size="small"
+            maxlength="50"
+          ></el-input>
         </el-form-item>
         <el-form-item label="副标题">
-          <el-input v-model="form.subname" size="small" maxlength="50"></el-input>
+          <el-input
+            v-model="form.subname"
+            size="small"
+            maxlength="50"
+          ></el-input>
         </el-form-item>
         <el-form-item label="图片">
-          <el-upload v-for="(item,index) in form.imgs" :key="index" class="avatar-uploader" action="/api/upload" :show-file-list="false" :on-success="(res, file)=>handleAvatarSuccess(res, file,index)" :before-upload="beforeAvatarUpload">
-            <form-img :img="item"></form-img>
-            <i class="imgs-del el-icon el-icon-delete" @click.stop="img_del_handle(index)"></i>
+          <div 
+            v-for="(item,index) in form.imgs"
+            :key="index"
+            class="avatar-uploader"
+            action="/api/upload"
+            :show-file-list="false"
+            :on-success="(res, file)=>handleAvatarSuccess(res, file,index)"
+            :before-upload="beforeAvatarUpload"
+          >
+            <div class="el-upload el-upload--text" style="cursor: default;">
+              <form-img :img="item"></form-img>
+              <i
+                class="imgs-del el-icon el-icon-delete"
+                @click.stop="img_del_handle(index)"
+              ></i>
+            </div>
+          </div>
+          <el-upload
+            class="avatar-uploader"
+            action="/api/upload"
+            :show-file-list="false"
+            :on-success="(res, file)=>handleAvatarSuccess(res, file,0)"
+            :before-upload="beforeAvatarUpload"
+          >
+            <i class="el-icon-plus avatar-uploader-icon1"></i>
           </el-upload>
-          <el-upload class="avatar-uploader" action="/api/upload" :show-file-list="false" :on-success="(res, file)=>handleAvatarSuccess(res, file,0)" :before-upload="beforeAvatarUpload">
-            <i class="el-icon-plus avatar-uploader-icon"></i>
-          </el-upload>
+          <div style="clear: both;color:#f00">
+            标准尺寸：411*192
+          </div>
         </el-form-item>
         <el-form-item label="薪资范围">
-          <el-col :span="4" style="min-width:190px;">
-            <el-input-number v-model="form.salaryStart" :min="1" :max="100000" label="起始薪资"></el-input-number>
+          <el-col
+            :span="4"
+            style="min-width:190px;"
+          >
+            <el-input-number
+              v-model="form.salaryStart"
+              :min="1"
+              :max="100000"
+              label="起始薪资"
+            ></el-input-number>
           </el-col>
-          <el-col style="text-align: center;" :span="1">-</el-col>
-          <el-col :span="4" style="min-width:190px;">
-            <el-input-number v-model="form.salaryEnd" :min="1" :max="100000" label="截止薪资"></el-input-number>
+          <el-col
+            style="text-align: center;"
+            :span="1"
+          >-</el-col>
+          <el-col
+            :span="4"
+            style="min-width:190px;"
+          >
+            <el-input-number
+              v-model="form.salaryEnd"
+              :min="1"
+              :max="100000"
+              label="截止薪资"
+            ></el-input-number>
           </el-col>
         </el-form-item>
         <el-form-item label="是否置顶">
-          <el-switch v-model="form.isTop" active-color="#13ce66" inactive-color="#ff4949" active-text="是" inactive-text="否">
+          <el-switch
+            v-model="form.isTop"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            active-text="是"
+            inactive-text="否"
+          >
           </el-switch>
         </el-form-item>
         <el-form-item label="类型">
-          <el-col :span="4" style="min-width:190px;padding-right: 10px;">
-            <el-select size="small" v-model="form.type">
-              <el-option label="无" :value="0"></el-option>
-              <el-option label="工价" :value="1"></el-option>
-              <el-option label="补贴" :value="2"></el-option>
+          <el-col
+            :span="4"
+            style="min-width:190px;padding-right: 10px;"
+          >
+            <el-select
+              size="small"
+              v-model="form.type"
+            >
+              <el-option
+                label="无"
+                :value="0"
+              ></el-option>
+              <el-option
+                label="工价"
+                :value="1"
+              ></el-option>
+              <el-option
+                label="补贴"
+                :value="2"
+              ></el-option>
             </el-select>
           </el-col>
-          <el-col v-if="form.type==2" :span="6" style="min-width:190px;">
-            <el-input v-model="form.subsidyExplain" size="small" maxlength="50" placeholder="补贴说明"></el-input>
+          <el-col
+            v-if="form.type==2"
+            :span="6"
+            style="min-width:190px;"
+          >
+            <el-input
+              v-model="form.subsidyExplain"
+              size="small"
+              maxlength="50"
+              placeholder="补贴说明"
+            ></el-input>
           </el-col>
-          <el-col v-if="form.type==1" :span="5">
-            <el-input-number size="small" v-model="form.laborPrice" :min="1" :max="999"></el-input-number><span style="margin-left:8px;">元/小时</span>
+          <el-col
+            v-if="form.type==1"
+            :span="5"
+          >
+            <el-input-number
+              size="small"
+              v-model="form.laborPrice"
+              :min="1"
+              :max="999"
+            ></el-input-number><span style="margin-left:8px;">元/小时</span>
           </el-col>
         </el-form-item>
         <el-form-item label="补贴">
-          <div v-for="(item,index) in form.subsidys" :key="index" class="subsidy-item">
+          <div
+            v-for="(item,index) in form.subsidys"
+            :key="index"
+            class="subsidy-item"
+          >
             <div>
-              <vue-editor :edit="form.subsidys[index].value" @changeedit="content=>form.subsidys[index].value = content"></vue-editor>
+              <vue-editor
+                :edit="form.subsidys[index].value"
+                @changeedit="content=>form.subsidys[index].value = content"
+              ></vue-editor>
             </div>
-            <div class="subsidy-close" @click="subsidy_close(index)"><i class="el-dialog__close el-icon el-icon-close"></i></div>
+            <div
+              class="subsidy-close"
+              @click="subsidy_close(index)"
+            ><i class="el-dialog__close el-icon el-icon-close"></i></div>
           </div>
-          <el-button type="success" size="small" @click="add_subsidys">添加补贴</el-button>
+          <el-button
+            type="success"
+            size="small"
+            @click="add_subsidys"
+          >添加补贴</el-button>
         </el-form-item>
         <el-form-item label="工资福利">
-          <vue-editor :edit="form.wages" @changeedit="content=>form.wages = content"></vue-editor>
+          <vue-editor
+            :edit="form.wages"
+            @changeedit="content=>form.wages = content"
+          ></vue-editor>
         </el-form-item>
         <el-form-item label="岗位介绍">
-          <vue-editor :edit="form.post" @changeedit="content=>form.post = content"></vue-editor>
+          <vue-editor
+            :edit="form.post"
+            @changeedit="content=>form.post = content"
+          ></vue-editor>
         </el-form-item>
         <el-form-item label="企业介绍">
-          <vue-editor :edit="form.introduce" @changeedit="content=>form.introduce = content"></vue-editor>
+          <vue-editor
+            :edit="form.introduce"
+            @changeedit="content=>form.introduce = content"
+          ></vue-editor>
         </el-form-item>
       </el-form>
     </el-card>
-    <div slot="footer" class="dialog-footer">
+    <div
+      slot="footer"
+      class="dialog-footer"
+    >
       <el-button @click="handleClose">取消</el-button>
-      <el-button type="primary" @click="confirm_handle">确认</el-button>
+      <el-button
+        type="primary"
+        @click="confirm_handle"
+      >确认</el-button>
     </div>
   </el-dialog>
 </template>
@@ -263,5 +391,10 @@ export default {
 }
 .subsidy-close:hover {
   color: #f00;
+}
+.avatar-uploader-icon1 {
+  font-size: 50px;
+  color: #8c939d;
+  text-align: center;
 }
 </style>
